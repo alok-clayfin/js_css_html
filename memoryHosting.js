e11ff27cce475b16ed0cd293ceea2d273f23954f
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-08-17 10:25:58 +0530

    Memory_Hosting_17/08/22

---
+++ memoryHosting.js
@@ -0,0 +1,14 @@
+console.log( myName, printName() );
+
+var myName = "Shree";
+
+function printName()
+{
+    console.log( a, embed() );
+    
+    var a = 100;
+    
+    function embed(){ return "hello"; }
+    
+    return "Alok";
+}
